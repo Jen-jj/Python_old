0f5200fd1d8decac95fd7ca89e80fb6880172ad5
Jenya Kissel <jenya.kissel@gmail.con>
2020-08-30 12:52:12 +0200

    2nd  commit test

--- test1.py
+++ test1.py
@@ -15,3 +15,22 @@ print(age)
 stuff1 = ['piano', 'dance', 'study']
 stuff2 = ['coffe', 'tiramisu', 'fig jam']
 print(stuff1 + stuff2)
+
+
+list2 = ['milk', 'eggs', 'strawberry jam']
+print(list2[2])
+list3 = [1, 2, 3, 4, 5, 6]
+print(list3[2:6])
+my_list = [list2, list3]
+list2.append('coffe')
+print(list2)
+del list3[2]
+print(list3 * 3)
+smt = (1, 7, 9 ,5)
+print(smt[2])
+age = {'m' : 13, 'h' : 14, 'j' : 12}
+age['m'] = 17
+print(age)
+stuff1 = ['piano', 'dance', 'study']
+stuff2 = ['coffe', 'tiramisu', 'fig jam']
+print(stuff1 + stuff2)
